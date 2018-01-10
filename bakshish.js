7f0fb660af93326a7da4d35bd94b0885dbdc498d
Nadav Ivgi <nadav@shesek.info>
2018-01-10 12:38:16 +0200

    Set default CHARGE_URL

--- bakshish.js
+++ bakshish.js
@@ -1,5 +1,5 @@
 // Initialize Lightning Charge client
-const charge = require('lightning-charge-client')(process.env.CHARGE_URL, process.env.CHARGE_TOKEN)
+const charge = require('lightning-charge-client')(process.env.CHARGE_URL || 'http://localhost:9112', process.env.CHARGE_TOKEN)
 
 // Express setup & settings
 const app = require('express')()
